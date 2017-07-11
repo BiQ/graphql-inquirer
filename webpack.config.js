ab1643e4010cf78fb597087d381ad0e7c9380a13
ViggoV <vefr86@gmail.com>
2017-07-11 18:30:30 +0200

    config

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var webpack = require('webpack');
+var path = require('path');
+
+
+var config = {
+
+  entry: {
+    application: './src/application.js'
+  },
+
+  output: {
+    path: __dirname+'/bin',
+    publicPath: './bin/',
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'CHANGE_ME'
+  },
+
+  resolve: {
+    extensions: ['.js','.jsx','.sass','.scss']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      },
+      {
+        test: /\.(jpe?g|png|git|svg|woff2?|eot|ttf)$/i,
+        exclude: /node_modules/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+
+    ]
+  },
+
+}
+
+module.exports = config;
