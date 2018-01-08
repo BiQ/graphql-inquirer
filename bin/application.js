72bf0bc0cfe6dc702e4d54ad94837131615bd92b
ViggoV <vefr86@gmail.com>
2018-01-08 15:20:33 +0100

    build commit

--- bin/application.js
+++ bin/application.js
@@ -17608,7 +17608,7 @@ module.exports = navigator && navigator.userAgent || '';
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
+/* WEBPACK VAR INJECTION */(function(process) {
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -17676,6 +17676,9 @@ var Inquirer = function (_React$Component) {
   _createClass(Inquirer, [{
     key: 'render',
     value: function render() {
+
+      console.log('process', process);
+
       var _state = this.state,
           loading = _state.loading,
           schema = _state.schema;
@@ -17725,7 +17728,7 @@ var Inquirer = function (_React$Component) {
 
       return _react2.default.createElement(
         _reactRouterDom.BrowserRouter,
-        null,
+        { basename: process.env.PUBLIC_URL },
         _react2.default.createElement(_reactRouterDom.Route, { path: '/', render: function render(routeProps) {
             return _react2.default.createElement(
               'div',
@@ -17759,6 +17762,7 @@ App
     - Split2: EditorGenerated
     - Split3: EditorResult
  */
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(11)))
 
 /***/ }),
 /* 210 */
