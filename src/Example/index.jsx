fb8ceeff14cb6e540d22e82ba5b0f9152860e8bf
ViggoV <vef@biq.dk>
2024-09-12 14:23:43 +0200

    npm audit fix + new example api url

--- src/Example/index.jsx
+++ src/Example/index.jsx
@@ -1,36 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route } from 'react-router-dom'
 
-import Inquirer from '../application.jsx';
+import Inquirer from '../application.jsx'
 
 const fetchFunc = (query) => {
-
   let req_opts = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(query),
-  };
+  }
 
   //let req_url = 'https://1jzxrj179.lp.gql.zone/graphql';
-  let req_url = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
+  let req_url = 'https://beta.pokeapi.co/graphql/v1beta'
 
-  return fetch(req_url, req_opts).then((response) => (response.json()));
-
-};
+  return fetch(req_url, req_opts).then((response) => response.json())
+}
 
 // An example using the router within the Inquirer
-const App = () => <Inquirer fetcher={fetchFunc} />; // eslint-disable-line no-unused-vars
+const App = () => <Inquirer fetcher={fetchFunc} /> // eslint-disable-line no-unused-vars
 
 // An example of putting Inquirer inside an existing router
 const RoutedApp = () => (
   <HashRouter>
-    <Route path="/stuff/" component={() =>
-      <Inquirer fetcher={fetchFunc} isInARouter />
-    } />
+    <Route
+      path="/stuff/"
+      component={() => <Inquirer fetcher={fetchFunc} isInARouter />}
+    />
   </HashRouter>
-);
+)
 
-window.onload = function(){
-  ReactDOM.render(<App />, document.getElementById('stage'));
-};
+window.onload = function () {
+  ReactDOM.render(<App />, document.getElementById('stage'))
+}
